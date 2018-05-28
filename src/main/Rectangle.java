d92cccaf47846c134839295832acf6a3ee333148
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-28 15:05:08 +0530

    shouldReturnZeroWhenLengthAndBreadthAreZero

---
+++ src/main/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    private int breadth;
+    private int length;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int calculateArea() {
+        return 0;
+    }
+}
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class RectangleTest {
+    @Test
+    public void shouldReturnZeroWhenLengthAndBreadthAreZero() {
+       Rectangle rectangle = new Rectangle(0,0);
+       assertEquals(0,rectangle.calculateArea());
+    }
 }
