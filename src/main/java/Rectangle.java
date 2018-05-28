d65a5eff2369cd683c23cfe337cd027e8c2ab758
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-28 16:53:36 +0530

    shouldReturnZeroAsPerimeterWhenLengthAndBreadthAreZero

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -3,11 +3,18 @@ public class Rectangle {
     private double length;
 
     public Rectangle(double length, double breadth) {
+        if(length < 0 || breadth < 0) {
+            throw new IllegalArgumentException();
+        }
         this.length = length;
         this.breadth = breadth;
     }
 
     public double calculateArea() {
         return length * breadth;
+    }
+
+    public double calculatePerimeter() {
+        return 0.0;
     }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -4,28 +4,30 @@ import static org.junit.Assert.assertEquals;
 
 
 public class RectangleTest {
+    Rectangle rectangle;
     private final double DELTA = 0.0001;
-    @Test
-    public void shouldReturnZeroAsAreaWhenLengthAndBreadthAreZero() {
-       Rectangle rectangle = new Rectangle(0,0);
-       assertEquals(0,rectangle.calculateArea(), DELTA);
-    }
 
     @Test
-    public void shouldReturnFourAsAreaWhenLengthAndBreathAreTwo() {
-        Rectangle rectangle = new Rectangle(2,2);
-        assertEquals(4,rectangle.calculateArea(), DELTA);
+    public void shouldReturnZeroAsAreaWhenLengthAndBreadthAreZero() {
+       rectangle = new Rectangle(0, 0);
+        assertEquals(0, rectangle.calculateArea(), DELTA);
     }
 
     @Test
     public void shouldReturnTwentyWhenLengthIsFourAndBreadthIsFive() {
-        Rectangle rectangle = new Rectangle(4,5);
-        assertEquals(20,rectangle.calculateArea(), DELTA);
+        rectangle = new Rectangle(4, 5);
+        assertEquals(20, rectangle.calculateArea(), DELTA);
     }
 
     @Test
     public void shouldReturnAreaForPointValues() {
-        Rectangle rectangle = new Rectangle(2.5,4.1);
-        assertEquals(10.25,rectangle.calculateArea(),DELTA);
+        rectangle = new Rectangle(2.5, 4.1);
+        assertEquals(10.25, rectangle.calculateArea(), DELTA);
+    }
+
+    @Test
+    public void shouldReturnZeroAsPerimeterWhenLengthAndBreadthAreZero() {
+        rectangle = new Rectangle(0,0);
+        assertEquals(0,rectangle.calculatePerimeter(),DELTA);
     }
 }
