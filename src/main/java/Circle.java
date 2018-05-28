a590a6f8592f25ba2c65fcb0659555790ef6abcc
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-28 21:50:52 +0530

    returnAreaWhenRadiusIsGiven

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,11 +1,12 @@
 public class Circle {
     private final double radius;
+    private final double PI = 3.14;
 
     public Circle(double radius) {
         this.radius = radius;
     }
 
     public double calculateRadius() {
-        return 0;
+        return PI * radius * radius;
     }
 }
--- src/test/java/CircleTest.java
+++ src/test/java/CircleTest.java
@@ -4,10 +4,18 @@ import static org.junit.Assert.assertEquals;
 
 public class CircleTest {
     private final double DELTA = 0.0001;
+
     @Test
-    public void returnZeroWhenRadiusIsZero(){
+    public void returnZeroAsAreaWhenRadiusIsZero(){
         Circle circle = new Circle(0);
 
         assertEquals(0,circle.calculateRadius(),DELTA);
+    }
+
+    @Test
+    public void returnAreaWhenRadiusIsGiven(){
+        Circle circle = new Circle(4);
+
+        assertEquals(50.24,circle.calculateRadius(),DELTA);
     }
 }
